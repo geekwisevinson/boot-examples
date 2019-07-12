9465d5af6d4a124c8089c303a0915daf948fc7b2
Vinson Fernandez <vinson.fernandez@admindirect.com>
2019-07-11 17:18:45 -0700

    update examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './headers/headers.component';
+import { Header1Component } from './header1/header1.component';
+import { JumbotronsComponent } from './jumbotrons/jumbotrons.component';
+import { Jumbotron1Component } from './jumbotrons/jumbotron1/jumbotron1.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { Button1Component } from './buttons/button1/button1.component';
+import { Button2Component } from './buttons/button2/button2.component';
+import { Button3Component } from './buttons/button3/button3.component';
+import { Button4Component } from './buttons/button4/button4.component';
+import { Button5Component } from './buttons/button5/button5.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    Header1Component,
+    JumbotronsComponent,
+    Jumbotron1Component,
+    ButtonsComponent,
+    Button1Component,
+    Button2Component,
+    Button3Component,
+    Button4Component,
+    Button5Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
